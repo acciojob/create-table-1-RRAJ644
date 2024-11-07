674924dfb40bd108e3a92a998dfc4e2baf73466b
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-07 16:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function insert_Row() {
-  // Get the table element
-  let table = document.querySelector('#sampleTable');
+  // Get the table body element
+  let tbody = document.querySelector('#sampleTable tbody');
   
   // Create a new row and define its content
   let newRow = document.createElement('tr');
   newRow.innerHTML = "<td>New Cell1</td><td>New Cell2</td>";
   
-  // Insert the new row at the top of the table
-  table.insertBefore(newRow, table.firstElementChild);
+  // Insert the new row at the top of the table body
+  tbody.insertBefore(newRow, tbody.firstElementChild);
 }
